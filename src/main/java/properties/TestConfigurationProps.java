d0791af17223214235ca2f7c2cfb724342a6a040
Manuel <tyranidas@hotmail.com>
2022-10-28 17:12:29 +0200

    first commit

---
+++ src/main/java/properties/TestConfigurationProps.java
@@ -0,0 +1,27 @@
+package properties;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class TestConfigurationProps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ResourceBundle config = ResourceBundle.getBundle("propriete");
+		
+		//String firstName = config.getString("database.driver");
+		
+		Enumeration <String> EnumConfig = config.getKeys();
+		
+		while (EnumConfig.hasMoreElements())
+		{
+			System.out.println(EnumConfig.nextElement());
+		}
+		
+		
+		
+		
+		
+	}
+
+}
